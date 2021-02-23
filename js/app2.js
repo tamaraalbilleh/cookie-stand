f18b3c711ca56c7a3c124544eca920609ad81493
tamaraalbilleh <tamara2albella@gmail.com>
2021-02-23 22:20:49 +0200

    almost finished with lab 8b

--- js/app2.js
+++ js/app2.js
@@ -1,36 +1,3 @@
 'use strick';
-{/* <header id="home-header">
-        <img id="logo" src="assets/logo.png" alt="logo">
-        <h1>cookie stand</h1>
-        <ul>
-            <li><a href="sales.html">Sales for our shops</a></li>
-        </ul>
-    </header> */}
 
-let header = {
-  logo : './assets/logo.png',
-  render : function (){
-    const parentElement = document.getElementById('home-header');
-
-    const imgElement = document.createElement ('img');
-    parentElement.appendChild(imgElement);
-    imgElement.setAttribute('src', this.logo);
-    imgElement.setAttribute ('id','logo');
-    imgElement.setAttribute ('alt','logo');
-
-    const h1Element = document.createElement('h1');
-    parentElement.appendChild (h1Element);
-    h1Element.textContent = 'cookie stand';
-
-    const ulElement = document.createElement('ul');
-    parentElement.appendChild (ulElement);
-    const aElement = document.createElement('a');
-    ulElement.appendChild (aElement);
-    aElement.textContent = 'Sales for our shops';
-    aElement.setAttribute ('href','sales.html');
-
-  }
-
-};
-header.render();
 
